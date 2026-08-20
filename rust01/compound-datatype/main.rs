35561d757521c08f31bb00942ffc16dce5c8f090
grah4na <syknandan@gmail.com>
2026-08-20 11:11:56 +0530

    done with primitive data types

--- rust01/compound-datatype/main.rs
+++ rust01/compound-datatype/main.rs
@@ -23,8 +23,35 @@ let humans:(String,i32,bool) = ("nandan".to_string(),43,true);
 
 println!("tuples : {:?}",humans);
 
+//slices :
+let numberslices : &[i32]= &[1,2,3,4,5];
+let animalslices : &[&str] = &["lion","tiger","elephant"];
+
+println!("slices : {:?}",numberslices);
+println!("slices : {:?}",animalslices);
+
+//stings : which are mutable ,growable and owned string type
+
+let mut travis : String = String :: from("loneliness has followed me my whole life,");
+//travis.push_str("everywhere"); here we cannot borrow as mutable , 
+travis.push_str("everywhere");
+println!("speak: {}",travis);
+
+//what is string slice :
+
+let string : String = String :: from ("hello world");
+let slice : &str = &string[0..5];
+
+println!("slice is :{}",slice)
+
 }
 
 
 
+// diff btw string ,string slice & slice
+// String` → Owns the text, stored on the heap, and can grow/shrink.
+//`&str` (string slice) → Borrows text; it’s a view into existing UTF-8 string data and doesn’t own it.
+//`&[T]` (slice) → Borrows a portion of an array/vector of any type `T`, without owning it.
+
+**//Simple analogy:** `String` = owning a book, `&str` = reading a page from it, `&[T]` = viewing part of any list.
 
