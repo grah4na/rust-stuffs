79dc5d32269909ec12f06bb9a9968b73e94dd2da
grah4na <syknandan@gmail.com>
2026-08-25 13:51:06 +0530

    done with borrwoing and reference

---
+++ rust01/ref-and-borrowing/src/main.rs
@@ -0,0 +1,46 @@
+// fn main(){
+//     let mut _x = 5;
+//     let _r = &mut _x;
+//     *_r += 1;
+//     println!("{}",_r);
+
+// }
+// here "_r = &x" means i am targeting the x , if "*_r " means targeting the value of x (which is 5)
+
+//demonstration of one mutable ref and many immutable ref
+
+fn main(){
+    let mut account =  BankAccount {
+    owner : "alice".to_string(),
+    bal : 150.55,
+    };
+// immutable borrow to check balance 
+account.check_bal();
+
+// muttable borrow to withdraw money 
+account.withdraw(45.5);
+
+account.check_bal();
+}
+struct BankAccount{
+    owner : String,
+    bal : f64,
+}
+
+impl BankAccount {
+    fn withdraw(& mut self,amt : f64){
+        println!("withdrawing {} from acc owned by {}", amt ,self.owner);
+        self.bal -= amt;
+    }
+
+    fn check_bal(&self){
+    
+        println!("acc owned by {} has a balance of {}", self.owner , self.bal);
+    }
+
+
+}
+
+// OUTPUT : acc owned by alice has a balance of 150.55
+// withdrawing 45.5 from acc owned by alice
+// acc owned by alice has a balance of 105.05000000000001
