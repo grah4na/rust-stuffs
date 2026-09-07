c3cdca2ca2a7842e6cb16674644401cb09d1ab69
grah4na <syknandan@gmail.com>
2026-09-07 13:13:50 +0530

    struct in rust

--- rust01/structs/src/main.rs
+++ rust01/structs/src/main.rs
@@ -79,3 +79,24 @@ fn main() {
 // fn area(rectangle: &Rectangle) -> u32 {
 //     rectangle.width * rectangle.height
 // }
+
+
+
+
+// rust will give some functionality to debug issue here are some
+
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let scale = 2;
+//     let rect1 = Rectangle {
+//         width: dbg!(30 * scale),
+//         height: 50,
+//     };
+
+//     dbg!(&rect1);
+// }
