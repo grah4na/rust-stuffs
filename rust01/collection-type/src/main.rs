8ad937a40762739ad6f7bea0060b5c48d807fa5a
grah4na <syknandan@gmail.com>
2026-09-10 09:37:31 +0530

    hashmap in collection type

--- rust01/collection-type/src/main.rs
+++ rust01/collection-type/src/main.rs
@@ -51,6 +51,40 @@ fn main() {
 
 
 
+    // how hash map works 
+
+
+       // use std::collections::HashMap;  we need to import the module from std library
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+
+
+    // accessing key values through loop
+
+        use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+
+
+    
+
+
+
 
 
 
